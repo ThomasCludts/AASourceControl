85bc7b1afb2843570542724a03f7c17bba22ddb5
ThomasCludts <36035768+ThomasCludts@users.noreply.github.com>
2018-02-01 12:23:07 +0100

    Update Start-SourceControl.ps1

--- Start-SourceControl.ps1
+++ Start-SourceControl.ps1
@@ -1,7 +1,8 @@
 workflow Start-SourceControl
 {
-    #v1.0
+    #v2.0
     #This is my first Source control Runbook
+    #adding a minor change in GitHub
 
-    write-output "Azure Automation"
+    write-output "Azure Automation source control via Github"
 }
